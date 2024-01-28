6ecbc74598cd90f6c93f0ae55794309a5a92d27d
Kirnovs <Kirnovs@mail.ru>
2024-01-28 16:35:52 +0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://6bb0f62b-ae13-49fa-a178-d3e97feda62a.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://566bc3a9-43e2-4127-adb6-43c247bb6813.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS_PATH = "/api/v1/kits"
